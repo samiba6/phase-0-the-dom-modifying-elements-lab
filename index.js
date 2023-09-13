1b42c7bc0ba50041c51a896457b809a5eca61df1
Sami <s.babouche@esi-sba.dz>
2023-09-13 01:40:20 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+main =document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.innerHTML="Sami is the champion"
